6f31bd95a9d97b610e57d0a67d256ee9ec63aaa9
TaviJ <tavierjohnson@gmail.com>
2026-02-17 16:32:30 -0700

    Add schema.sql for Postgres

---
+++ schema.sql
@@ -0,0 +1,31 @@
+
+
+BEGIN;
+
+
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  hashed_password TEXT NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS log_entries (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+
+  entry_date DATE,
+  mood TEXT,
+  activity TEXT,
+  note TEXT,
+  is_public BOOLEAN NOT NULL DEFAULT FALSE,
+
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+
+CREATE INDEX IF NOT EXISTS idx_log_entries_user_id ON log_entries(user_id);
+CREATE INDEX IF NOT EXISTS idx_log_entries_entry_date ON log_entries(entry_date);
+
+COMMIT;

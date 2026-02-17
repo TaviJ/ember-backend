938988ce39a1467a4fa08c06a30ae0f0a7365898
TaviJ <tavierjohnson@gmail.com>
2026-02-17 14:23:07 -0700

    preparing for deployment

--- app.py
+++ app.py
@@ -17,13 +17,20 @@ CORS(app)
 # Database Connection
 # ---------------------------
 def get_db_connection():
-    connection = psycopg2.connect(
-        host='localhost',
-        database='ember',
-        user=os.getenv('POSTGRES_USERNAME'),
-        password=os.getenv('POSTGRES_PASSWORD')
-    )
+    if 'ON_HEROKU' in os.environ:
+        connection = psycopg2.connect(
+            os.getenv('DATABASE_URL'), 
+            sslmode='require'
+        )
+    else:
+        connection = psycopg2.connect(
+            host='localhost',
+            database=os.getenv('POSTGRES_DATABASE'),
+            user=os.getenv('POSTGRES_USERNAME'),
+            password=os.getenv('POSTGRES_PASSWORD')
+        )
     return connection
+
 
 
 def to_bool(value):
@@ -333,4 +340,4 @@ def public_log_entries_index():
 # RUN SERVER
 # ---------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
